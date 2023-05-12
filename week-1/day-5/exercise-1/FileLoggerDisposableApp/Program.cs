326704d50df477cf69dd0ad998863eced707db6e
omprakash-abhyankar <omabhyankar03@gmail.com>
2023-05-12 18:23:01 +0530

    FileLoggerDisposableApp

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace FileLoggerDisposableApp
-{
+{{
     internal class Program
     {
         static void Main(string[] args)
         {
             // Use FileLogger and dispose of it properly
+            FileLogger fl = new FileLogger("om.txt");
+            fl.Log("Bonjourno\n");
+            fl.Log("My Name Is Om\n");
+            fl.Dispose();
+
+            // After using fl.Dispose(), I wasn't able to write to the same file as my _writer property was disposed successfully.
+
+            fl = new FileLogger("Omprakash.txt");
+            fl.Log("Hello World");
+            fl.Dispose();
         }
     }
 
@@ -15,16 +25,19 @@
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _writer = new StreamWriter(filePath);
         }
 
         public void Dispose()
         {
             // Implement IDisposable pattern
+            _writer.Dispose();
         }
 
         public void Log(string message)
         {
             // Write message to the file
+            _writer.Write(message);
         }
     }
 }
--- week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
+++ week-1/day-5/exercise-3/AsyncAwaitBasics/Program.cs
@@ -5,16 +5,38 @@
         static async Task Main(string[] args)
         {
             // Call PerformCalculations and measure time taken using Stopwatch
+            int numberOfTasks = 5;
+            Console.WriteLine($"Starting {numberOfTasks} long-running tasks...");
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            await PerformCalculations(numberOfTasks);
+            stopwatch.Stop();
+            Console.WriteLine($"Time taken to complete all tasks: {stopwatch.Elapsed}");
+
         }
 
         static async Task SimulateLongRunningTask(int delayInSeconds)
         {
             // Implement long-running task simulation
+            Console.WriteLine($"Starting long-running task with delay of {delayInSeconds} seconds...");
+            await Task.Delay(TimeSpan.FromSeconds(delayInSeconds));
+            Console.WriteLine($"Long-running task with delay of {delayInSeconds} seconds completed.");
+
         }
 
         static async Task PerformCalculations(int numberOfTasks)
         {
             // Start long-running tasks concurrently and wait for them to complete
+
+            Console.WriteLine($"Starting {numberOfTasks} long-running tasks...");
+            Task[] tasks = new Task[numberOfTasks];
+            for (int i = 0; i < numberOfTasks; i++)
+            {
+                tasks[i] = SimulateLongRunningTask(i + 1);
+            }
+            await Task.WhenAll(tasks);
+            Console.WriteLine($"All {numberOfTasks} long-running tasks completed.");
+
+
         }
     }
 }
